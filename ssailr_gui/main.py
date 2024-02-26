40d5c77c7db85e82ca6c61f3266b7cd169076cbf
vboxuser <vboxuser@Pramesh-VM.myguest.virtualbox.org>
2024-02-25 21:41:54 -0800

    Added help functionality

--- ssailr_gui/main.py
+++ ssailr_gui/main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QFileDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QFileDialog, QMessageBox
 from PyQt5.QtGui import QFont
 
 class MainMenu(QWidget):
@@ -19,7 +19,12 @@ class MainMenu(QWidget):
         options = ["Run All", "Use EasyDIVER", "Calculate Enrichment Statistics", "Figures and Misc.", "Help", "Quit"]
         for option in options:
             button = QPushButton(option, self)
-            button.clicked.connect(self.exit_application)
+            if option == "Help":
+                button.clicked.connect(self.display_help_message) 
+
+            if option == "Quit":
+                button.clicked.connect(self.exit_application)
+
             layout.addWidget(button)
 
         self.setLayout(layout)
@@ -37,8 +42,32 @@ class MainMenu(QWidget):
     def display_figures(self):
         pass
 
-    def help(self):
-        pass
+    def display_help_message(self):
+        help_text = """
+        SimpleSAILR is a Bash program that takes input files of SELEX round reads in fastq(.gz) format and analyzes them, providing:
+        - Sequence type and length distribution
+        - Joined reads
+        - Enrichment information
+        There are also options to visualize sequence distribution, enrichment, and diversity data.
+
+        Version 2.0
+        Author: Allison Tee and Celia Blanco
+        Contact: ateecup@stanford.edu or celia.blanco@bmsis.org
+
+        REQUIRED:
+        - Input directory filepath
+
+        OPTIONAL MODIFIERS:
+        - Output directory filepath
+        - Forward primer sequence for extraction
+        - Reverse primer sequence for extraction
+        - Translating to amino acids
+        - Retaining individual lane outputs
+        - Number of threads
+        - Extra flags for PANDASeq (use quotes, e.g. "-L 50")
+        """
+
+        QMessageBox.information(self, "Help", help_text)
 
     def exit_application(self):
         QApplication.quit()

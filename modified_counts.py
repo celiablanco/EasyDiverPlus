d299780e8c8c9c8e5788a429fa68daaf01b82bf2
Pramesh Sharma <prameshsharma25@gmail.com>
2024-04-15 19:59:23 -0700

    Updated res file descriptor to csv

--- modified_counts.py
+++ modified_counts.py
@@ -7,6 +7,7 @@ import os
 import re
 import glob
 import fnmatch
+import csv
 
 from bootstrap import bootstrap
 
@@ -68,7 +69,7 @@ def run_enrichment_analysis(out_file, in_file=None, res_file=None, neg_file=None
 
     # Set default output file name if not provided
     if res_file is None:
-        res_file = "results.txt"
+        res_file = "results.csv"
 
     if not os.path.exists(os.path.split(res_file)[0]):
         os.makedirs(os.path.split(res_file)[0])
@@ -128,142 +129,126 @@ def run_enrichment_analysis(out_file, in_file=None, res_file=None, neg_file=None
 
     print("Max length sequence:", max_len)
 
-    out = open(res_file,'w')
-    print(str('number of unique sequences = ') + str(uniques[-1]), file=out) # Changed to -1
-    print(str('total number of molecules = ') + str(totals[-1]), end='\n', file=out)
-    if in_file is not None:
-        print(str('number of unique sequences (input) = ') + str(uniques[0]), file=out)
-        print(str('total number of molecules (input) = ') + str(totals[0]), end='\n', file=out)
-    if neg_file is not None:
-        print(str('number of unique sequences (neg control) = ') + str(uniques[1]), file=out)
-        print(str('total number of molecules (neg control) = ') + str(totals[1]), end='\n', file=out)
+    with open(res_file, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
 
-    print(str('seq').ljust(max_len), end='\t', file=out)
-    print(str('a_in').ljust(5), end='\t\t', file=out)
-    print(str('a_in (95 CI)').ljust(5), end='\t\t', file=out)
-    print(str('f_in').ljust(5), end='\t\t', file=out)
-    print(str('f_in (95 CI)').ljust(10), end='\t\t', file=out)
-    print(str(str('a_out')).ljust(5), end='\t\t', file=out)
-    print(str('a_out (95 CI)').ljust(10), end='\t\t', file=out)
-    print(str(str('f_out')).ljust(10), end='\t\t', file=out)
-    print(str(str('f_out (95 CI)')).ljust(10), end='\t\t', file=out)
-    if neg_file is not None:
-        print(str(str('a_neg')).ljust(5), end='\t\t', file=out)
-        print(str(str('a_neg (95 CI)')).ljust(10), end='\t\t', file=out)
-        print(str(str('f_neg')).ljust(10), end='\t\t', file=out)
-        print(str(str('f_neg (95 CI)')).ljust(10), end='\t\t', file=out)
-        print(str(str('e_out')).ljust(10), end='\t\t', file=out)
-        print(str(str('e_out (interval)')).ljust(10), end='\t\t', file=out)
-    if neg_file is not None:
-        print(str(str('e_n')).ljust(10), end='\t\t', file=out)
-        print(str(str('e_n (interval)')).ljust(10), end='\t\t', file=out)
-        print(str(str(str('e_out')) + ("/") + str(str('e_n'))).ljust(10), end='\t\t', file=out)
-        print(str(str(str('e_out')) + ("/") + str(str('e_n (interval)'))).ljust(10), end='\n', file=out)
-    else:
-        print("\n")
-
-    for seq in all_dict[-1]: # Originally 2. Calculate each sequence's a_in, f_in, a_out, etc. stats
-        f_post = all_dict[-1][seq][1]
-        c_post = all_dict[-1][seq][0]
-
-        try:
-            f_in = all_dict[0][seq][1]
-            c_in = all_dict[0][seq][0]
-
-        except KeyError:
-            f_in = 0  # Change this line to modify fraction used if not found
-            c_in = 0
+        # Write general information
+        writer.writerow(['Number of Unique Sequences', uniques[-1]])
+        writer.writerow(['Total Number of Molecules', totals[-1]])
+        if in_file is not None:
+            writer.writerow(['Number of Unique Sequences (Input)', uniques[0]])
+            writer.writerow(['Total Number of Molecules (Input)', totals[0]])
         if neg_file is not None:
+            writer.writerow(['Number of Unique Sequences (Neg Control)', uniques[1]])
+            writer.writerow(['Total Number of Molecules (Neg Control)', totals[1]])
+
+        # Write column headers
+        writer.writerow(['seq', 'a_in', 'a_in (95 CI)', 'f_in', 'f_in (95 CI)', 'a_out', 'a_out (95 CI)', 'f_out', 'f_out (95 CI)', 'a_neg', 'a_neg (95 CI)', 'f_neg', 'f_neg (95 CI)', 'e_out', 'e_out (interval)', 'e_n', 'e_n (interval)', 'e_out/e_n', 'e_out/e_n (interval)'])
+
+        for seq in all_dict[-1]: # Originally 2. Calculate each sequence's a_in, f_in, a_out, etc. stats
+            f_post = all_dict[-1][seq][1]
+            c_post = all_dict[-1][seq][0]
+
             try:
-                f_neg = all_dict[1][seq][1]
-                c_neg = all_dict[1][seq][0]
+                f_in = all_dict[0][seq][1]
+                c_in = all_dict[0][seq][0]
+
             except KeyError:
+                f_in = 0  # Change this line to modify fraction used if not found
+                c_in = 0
+            if neg_file is not None:
+                try:
+                    f_neg = all_dict[1][seq][1]
+                    c_neg = all_dict[1][seq][0]
+                except KeyError:
+                    f_neg = 0
+                    c_neg = 0
+            else:
                 f_neg = 0
                 c_neg = 0
-        else:
-            f_neg = 0
-            c_neg = 0
 
-        # Bootstrap data !!! Changed this part to make freq ranges make more sense according to abundances
-        c_post_boot = bootstrap(c_post, totals[2])
-        c_in_boot = bootstrap(c_in, totals[0])
-        c_neg_boot = None
+            # Bootstrap data !!! Changed this part to make freq ranges make more sense according to abundances
+            c_post_boot = bootstrap(c_post, totals[2])
+            c_in_boot = bootstrap(c_in, totals[0])
+            c_neg_boot = None
 
-        c_post_range = [0, 0] if c_post == 0 else [max(c_post - c_post_boot[1], 1), c_post + c_post_boot[1]]
-        f_post_range = [(x / float(totals[2])) for x in c_post_range]
-        c_in_range = [0, 0] if c_in == 0 else [max(c_in - c_in_boot[1], 1), c_in + c_in_boot[1]]
-        f_in_range = [(x / float(totals[0])) for x in c_in_range]
+            c_post_range = [0, 0] if c_post == 0 else [max(c_post - c_post_boot[1], 1), c_post + c_post_boot[1]]
+            f_post_range = [(x / float(totals[2])) for x in c_post_range]
+            c_in_range = [0, 0] if c_in == 0 else [max(c_in - c_in_boot[1], 1), c_in + c_in_boot[1]]
+            f_in_range = [(x / float(totals[0])) for x in c_in_range]
 
-    # Write data to file
-    unique_sequences = {}
-    seq_names = all_dict[-1].keys()
-    sequence_number = 1
-    chars58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+    # Write data to the CSV file
+        unique_sequences = {}
+        seq_names = all_dict[-1].keys()
+        sequence_number = 1
+        chars58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
-    for seq in seq_names:
-        if seq in unique_sequences.keys():
-            print("Found \"" + seq + "\" " + format_bootstrap(c_post_range, 'a') + " times with " + format_bootstrap(f_post_range, 'f') + " frequency.")
-        else:
-            seq_name_base58 = base_encode(sequence_number, chars58)
-            unique_sequences[seq] = seq_name_base58
-            sequence_number += 1
-            print(str(unique_sequences[seq]).ljust(max_len), end='\t', file=out)
-            print(str(c_in).ljust(10), end='\t', file=out)
-            print(format_bootstrap(c_in_range, 'a').ljust(15), end='\t', file=out)
-            print(str(f"{f_in:.6f}").ljust(10), end='\t', file=out)
-            print(format_bootstrap(f_in_range, 'f').ljust(15), end='\t', file=out)
-            print(str(c_post).ljust(10), end='\t', file=out)
-            print(format_bootstrap(c_post_range, 'a').ljust(15), end='\t', file=out)
-            print(str(f"{f_post:.6f}").ljust(10), end='\t', file=out)
-            print(format_bootstrap(f_post_range, 'f').ljust(15), end='\t', file=out)
-
-            if neg_file is not None:
-                c_neg_boot = bootstrap(c_neg, totals[1])
-                c_neg_range = [0, 0] if c_neg == 0 else [max(c_neg - c_neg_boot[1], 1), c_neg + c_neg_boot[1]]
-                f_neg_range = [(x / float(totals[2])) for x in c_neg_range]
-                print(str(c_neg).ljust(10), end='\t', file=out)
-                print(str(format_bootstrap(c_neg_range, 'a')).ljust(15), end='\t', file=out)
-                print(str(f"{f_neg:.6f}").ljust(10), end='\t', file=out)
-                print(format_bootstrap(f_neg_range, 'f').ljust(15), end='\t', file=out)
-
-            # !!! Calculate and adjust enrichment in positive and negative pools
-            if f_in_range[0] > 0: # If the max is more than 1, we've set the min to more than 1
-                enr_post_min = f_post_range[0] / f_in_range[1]  # Min enrichment due to selection - assumes smallest f_out and largest f_in
-                enr_post_max = f_post_range[1] / f_in_range[0]  # Max enrichment due to selection - assumes largest f_out and smallest f_in
-                enr_neg_min = f_neg_range[0] / f_in_range[1]
-                enr_neg_max = f_neg_range[1] / f_in_range[0]
-            else: # Not enough data to make an estimate
-                enr_post_min = 0
-                enr_post_max = 0
-                enr_neg_min = 0
-                enr_neg_max = 0
-
-            if enr_post_max > 0: # Makes sense to print enr_post
-                enr_post = f_post / f_in
-                print(str(f"{enr_post:.6f}").ljust(10), end='\t\t', file=out)
-                print(str(f"[{enr_post_min:.6f}, {enr_post_max:.6f}]").ljust(15), end='\t\t', file=out)
+        for seq in seq_names:
+            row = []
+            if seq in unique_sequences.keys():
+                print("Found \"" + seq + "\" " + format_bootstrap(c_post_range, 'a') + " times with " + format_bootstrap(f_post_range, 'f') + " frequency.")
             else:
-                print('-'.ljust(15), end='\t\t', file=out)
+                seq_name_base58 = base_encode(sequence_number, chars58)
+                unique_sequences[seq] = seq_name_base58
+                sequence_number += 1
+                row.append(unique_sequences[seq])
+                row.append(str(c_in))
+                row.append(format_bootstrap(c_in_range, 'a'))
+                row.append(str(f"{f_in:.6f}"))
+                row.append(format_bootstrap(f_in_range, 'f'))
+                row.append(str(c_post))
+                row.append(format_bootstrap(c_post_range, 'a'))
+                row.append(str(f"{f_post:.6f}"))
+                row.append(format_bootstrap(f_post_range, 'f'))
 
-            if neg_file is not None: # 2A, 2B case check
-                if enr_neg_max > 0:
-                    enr_neg = f_neg / f_in
-                    print(str(f"{enr_neg:.6f}").ljust(10), end='\t\t', file=out)
-                    print(str(f"[{enr_neg_min:.6f}, {enr_neg_max:.6f}]").ljust(15), end='\t\t', file=out)
+                if neg_file is not None:
+                    c_neg_boot = bootstrap(c_neg, totals[1])
+                    c_neg_range = [0, 0] if c_neg == 0 else [max(c_neg - c_neg_boot[1], 1), c_neg + c_neg_boot[1]]
+                    f_neg_range = [(x / float(totals[2])) for x in c_neg_range]
+                    row.append(str(c_neg))
+                    row.append(str(format_bootstrap(c_neg_range, 'a')))
+                    row.append(str(f"{f_neg:.6f}"))
+                    row.append(format_bootstrap(f_neg_range, 'f'))
+
+                # Calculate and adjust enrichment in positive and negative pools
+                if f_in_range[0] > 0: # If the max is more than 1, we've set the min to more than 1
+                    enr_post_min = f_post_range[0] / f_in_range[1]  # Min enrichment due to selection - assumes smallest f_out and largest f_in
+                    enr_post_max = f_post_range[1] / f_in_range[0]  # Max enrichment due to selection - assumes largest f_out and smallest f_in
+                    enr_neg_min = f_neg_range[0] / f_in_range[1]
+                    enr_neg_max = f_neg_range[1] / f_in_range[0]
+                else: # Not enough data to make an estimate
+                    enr_post_min = 0
+                    enr_post_max = 0
+                    enr_neg_min = 0
+                    enr_neg_max = 0
+
+                if enr_post_max > 0: # Makes sense to print enr_post
+                    enr_post = f_post / f_in
+                    row.append(str(f"{enr_post:.6f}"))
+                    row.append(str(f"[{enr_post_min:.6f}, {enr_post_max:.6f}]"))
                 else:
-                    print('-'.ljust(15), end='\t\t', file=out)
+                    row.append('-')
 
-            if enr_neg_max > 0 and enr_neg_min > 0:
-                enr_ratio_min = enr_post_min / enr_neg_max
-                enr_ratio_max = enr_post_max / enr_neg_min
-                print(str(f"{enr_post / enr_neg:.6f}").ljust(10), end='\t\t', file=out)
-                print(str(f"[{enr_ratio_min:.6f}, {enr_ratio_max:.6f}]").ljust(15), end='\n', file=out)
-            elif neg_file is None:
-                print(' '.ljust(15), file=out)
-            else:
-                print('-'.ljust(15), file=out)
+                if neg_file is not None: # 2A, 2B case check
+                    if enr_neg_max > 0:
+                        enr_neg = f_neg / f_in
+                        row.append(str(f"{enr_neg:.6f}"))
+                        row.append(str(f"[{enr_neg_min:.6f}, {enr_neg_max:.6f}]"))
+                    else:
+                        row.append('-')
 
-    out.close()
+                if enr_neg_max > 0 and enr_neg_min > 0:
+                    enr_ratio_min = enr_post_min / enr_neg_max
+                    enr_ratio_max = enr_post_max / enr_neg_min
+                    row.append(str(f"{enr_post / enr_neg:.6f}"))
+                    row.append(str(f"[{enr_ratio_min:.6f}, {enr_ratio_max:.6f}]"))
+                elif neg_file is None:
+                    row.append(' ')
+                else:
+                    row.append('-')
+                
+                writer.writerow(row)
+
     print("Time elapsed: " + str(time() - start) + ' s')
 
 def find_enrichments():
@@ -316,9 +301,9 @@ def find_enrichments():
             # Run the modified_counts_bash.sh script with the appropriate arguments
             neg_files_exist = any(fnmatch.fnmatch(file, neg_format) for file in os.listdir(counts_dir))
             if not neg_files_exist:
-                run_enrichment_analysis(out_file=glob.glob(os.path.join(counts_dir, str(i) + "-out*" + "_" + counts_type + ".txt")), res_file=f"modified_counts/{i}-res.txt")
+                run_enrichment_analysis(out_file=glob.glob(os.path.join(counts_dir, str(i) + "-out*" + "_" + counts_type + ".txt")), res_file=f"modified_counts/{i}-res.csv")
             else:
-                run_enrichment_analysis(out_file=glob.glob(os.path.join(counts_dir, str(i) + "-out*" + "_" + counts_type + ".txt")), neg_file=glob.glob(os.path.join(counts_dir, str(i + 1) + "-neg*" + "_" + counts_type + ".txt")), res=glob.glob(os.path.join(outdir, "modified_counts", str(i) + "-res.txt")))
+                run_enrichment_analysis(out_file=glob.glob(os.path.join(counts_dir, str(i) + "-out*" + "_" + counts_type + ".txt")), neg_file=glob.glob(os.path.join(counts_dir, str(i + 1) + "-neg*" + "_" + counts_type + ".txt")), res=glob.glob(os.path.join(outdir, "modified_counts", str(i) + "-res.csv")))
             
             # Calculate progress
             progress = i * 100 / (max_round - 1)
@@ -329,9 +314,9 @@ def find_enrichments():
             # Run the modified_counts_bash.sh script with the appropriate arguments
             neg_files_exist = any(fnmatch.fnmatch(file, neg_format) for file in os.listdir(counts_dir))
             if not neg_files_exist:
-                run_enrichment_analysis(out_file=glob.glob(os.path.join(counts_dir, str(i) + "-out*" + "_" + counts_type + ".txt"))[0], in_file=glob.glob(os.path.join(counts_dir, str(i) + "-in*" + "_" + counts_type + ".txt")[0], res_file=f"modified_counts/{i}-res.txt"))
+                run_enrichment_analysis(out_file=glob.glob(os.path.join(counts_dir, str(i) + "-out*" + "_" + counts_type + ".txt"))[0], in_file=glob.glob(os.path.join(counts_dir, str(i) + "-in*" + "_" + counts_type + ".txt")[0], res_file=f"modified_counts/{i}-res.csv"))
             else:
-                run_enrichment_analysis(out_file=glob.glob(os.path.join(counts_dir, str(i) + "-out*" + "_" + counts_type + ".txt"))[0], in_file=glob.glob(os.path.join(counts_dir, str(i) + "-in*" + "_" + counts_type + ".txt"))[0], neg_file=glob.glob(os.path.join(counts_dir, str(i) + "-neg*" + "_" + counts_type + ".txt"))[0], res_file=f"{os.path.join(outdir, 'modified_counts', str(i) + '-res.txt')}")
+                run_enrichment_analysis(out_file=glob.glob(os.path.join(counts_dir, str(i) + "-out*" + "_" + counts_type + ".txt"))[0], in_file=glob.glob(os.path.join(counts_dir, str(i) + "-in*" + "_" + counts_type + ".txt"))[0], neg_file=glob.glob(os.path.join(counts_dir, str(i) + "-neg*" + "_" + counts_type + ".txt"))[0], res_file=f"{os.path.join(outdir, 'modified_counts', str(i) + '-res.csv')}")
 
         progress = i * 100 / max_round
         print("(Approx.) Progress:", progress, "%")
